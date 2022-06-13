7bbcae6e030ee5cf29aebdecfa02f7187de040f5
roberromero <robertoromerodeveloper@gmail.com>
2022-06-13 10:05:19 +0100

    Updates

---
+++ precache-manifest.28ead111a2c6051f2c6224827456bff7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7a1f50e6fbba32f530e",
+    "url": "/movie-search/static/css/main.89c453b7.chunk.css"
+  },
+  {
+    "revision": "f7a1f50e6fbba32f530e",
+    "url": "/movie-search/static/js/main.f7a1f50e.chunk.js"
+  },
+  {
+    "revision": "b0218314c019ff169711",
+    "url": "/movie-search/static/js/1.b0218314.chunk.js"
+  },
+  {
+    "revision": "05c144d15085a2fa07d3",
+    "url": "/movie-search/static/js/runtime~main.05c144d1.js"
+  },
+  {
+    "revision": "5ed4a03395ac7c9ed18dd6f250c67f34",
+    "url": "/movie-search/static/media/add.5ed4a033.png"
+  },
+  {
+    "revision": "caaf565c431869a4a1c1f28d8907ae28",
+    "url": "/movie-search/static/media/video.caaf565c.png"
+  },
+  {
+    "revision": "291c5de0e212cf43297baf9b6f24680c",
+    "url": "/movie-search/static/media/home.291c5de0.png"
+  },
+  {
+    "revision": "dbcb48ccede5468e2d89fcc5681e8d0b",
+    "url": "/movie-search/static/media/user.dbcb48cc.png"
+  },
+  {
+    "revision": "62579f730d7e839ba01c030c604164a3",
+    "url": "/movie-search/static/media/bin.62579f73.png"
+  },
+  {
+    "revision": "eec1fed9b99eafdc521c794c0314d856",
+    "url": "/movie-search/static/media/edit.eec1fed9.png"
+  },
+  {
+    "revision": "f86103652206dbfed9d8274430fee030",
+    "url": "/movie-search/static/media/github.f8610365.png"
+  },
+  {
+    "revision": "69d4516f42484d91755a338099d42ccf",
+    "url": "/movie-search/static/media/linkedin.69d4516f.png"
+  },
+  {
+    "revision": "6092afdadd32117cf50878c4a6526d70",
+    "url": "/movie-search/index.html"
+  }
+];
