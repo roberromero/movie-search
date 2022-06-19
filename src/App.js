1348545736429f413fcad6140813edc0d05475ab
roberromero <robertoromerodeveloper@gmail.com>
2022-06-19 14:02:19 +0100

    test12

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ const App = () => {
           <Route path='movie-search' element={movies && <Home api={API_URL} movies={movies} handleClick={handleClick}/>}/>
           <Route 
           path='/user' 
-          element={anyMovies ? <User data={data} updateData={updateData} decreaseMovieCounter={decreaseMovieCounter}/> : <Navigate to='/'/>}
+          element={anyMovies ? <User data={data} updateData={updateData} decreaseMovieCounter={decreaseMovieCounter}/> : <Navigate to='/movie-search'/>}
            />
           <Route path='*' element={ <ErrorPage /> }></Route>
         </Routes>
